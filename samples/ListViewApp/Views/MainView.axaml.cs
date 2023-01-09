1b07d0908888bef10d7449ef840c01247844baaa
Martijn Sneijders <martijn@florisoft.nl>
2023-01-09 20:25:32 +0100

    enkele listview

--- samples/ListViewApp/Views/MainView.axaml.cs
+++ samples/ListViewApp/Views/MainView.axaml.cs
@@ -12,7 +12,7 @@ namespace ListViewApp.Views
             btnDeselect.Click += BtnDeselect_Click;
             btnTest.Click += BtnTest_Click;
 
-            list2.SelectionChanged += List_SelectionChanged;
+            //list2.SelectionChanged += List_SelectionChanged;
         }
 
         protected override void OnLoaded()
@@ -25,43 +25,22 @@ namespace ListViewApp.Views
         protected override Size ArrangeOverride(Size finalSize)
         {
             var result = base.ArrangeOverride(finalSize);
-            bottomSheet.Width = result.Width / 2.0;
-            bottomSheet2.Width = result.Width / 2.0;
-            Canvas.SetLeft(bottomSheet2, result.Width / 2.0);
             return result;
         }
 
         private void List_SelectionChanged(object? sender, SelectionChangedEventArgs e)
         {
-            if (list2.SelectedItem is null)
-            {
-                bottomSheet.Classes.Clear();
-                bottomSheet2.Classes.Clear();
-            }
-            else
-            {
-                if(!bottomSheet.Classes.Contains("open"))
-                {
-                    bottomSheet.Classes.Add("open");
-                    bottomSheet2.Classes.Add("open");
-                }
-            }
             
         }
 
         private void BtnTest_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
         {
-            //bottomSheet.RenderTransformOrigin = new Avalonia.RelativePoint();
-            bottomSheet.Classes.Clear();
-            bottomSheet.Classes.Add("open");
 
-            bottomSheet2.Classes.Clear();
-            bottomSheet2.Classes.Add("open");
         }
 
         private void BtnDeselect_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
         {
-            list2.UnselectAll();
+
         }
     }
 }
