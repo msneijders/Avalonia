92588a7c451b1416cc716527eeb734e653d99532
Martijn Sneijders <martijn@florisoft.nl>
2023-01-23 20:54:41 +0100

    opgeruimd

--- samples/ListViewApp/Controls/StretchEdgeEffect.cs
+++ samples/ListViewApp/Controls/StretchEdgeEffect.cs
@@ -1,17 +1,16 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Numerics;
-using System.Xml.Linq;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
-using Avalonia.Media;
-using Avalonia.Rendering;
 using Avalonia.Rendering.Composition;
 using Avalonia.Threading;
 
 namespace ListViewApp.Controls;
 
+/// <summary>
+/// Will stretch the content of this control when 'overscrolling'. Currently only in vertical orientation.
+/// </summary>
 public class StretchEdgeEffect : Decorator
 {
     StretchEdgeEffectController? _controller;
@@ -36,22 +35,18 @@ public class StretchEdgeEffect : Decorator
     private class StretchEdgeEffectController
     {
         private readonly Visual _child;
+        private readonly double _maxStretchPercentage = 1.05; // 5% stretching max
+        private readonly Avalonia.Animation.Easings.Easing _easing = new Avalonia.Animation.Easings.CubicEaseOut();
+        private readonly TimeSpan releaseAnimationDuration = TimeSpan.FromMilliseconds(350);
 
         private double _stretch_y = 1.0;
         private bool _isStretched = false;
         private bool _isReleasing = false;
         private bool _stretchedBottom = false;
-        private readonly Stopwatch _st = Stopwatch.StartNew();
-        private readonly IRenderTimer _renderTimer;
-        private readonly Avalonia.Animation.Easings.Easing _easing = new Avalonia.Animation.Easings.CubicEaseOut();
-
-        private bool _needsUpdate = true;
-        public bool NeedsUpdate => _needsUpdate;
 
         public StretchEdgeEffectController(Visual child)
         {
             _child = child;
-            _renderTimer = AvaloniaLocator.Current.GetService<IRenderTimer>()!;
         }
 
         public void OnScrollGesture(object? sender, ScrollGestureEventArgs e)
@@ -62,30 +57,34 @@ public class StretchEdgeEffect : Decorator
                 return;
             }
 
+            StretchVertically(e);
+        }
+
+        public void OnScrollGestureEnded(object? sender, ScrollGestureEndedEventArgs e)
+        {
+            ReleaseStretch();
+        }
+
+        private void StretchVertically(ScrollGestureEventArgs e)
+        {
             if (_isReleasing)
                 return;
 
             if (e.Delta.Y == 0)
                 return;
 
-            var easing = new Avalonia.Animation.Easings.CubicEaseOut();
-            var ease = easing.Ease((_stretch_y - 1.0) * 20.0);
+            var ease = _easing.Ease((_stretch_y - 1.0) * (1.0 / (_maxStretchPercentage - 1.0)));
 
-            _stretch_y += Math.Abs(e.Delta.Y) / (1000.0 + (ease * 18000.0));
+            _stretch_y += Math.Abs(e.Delta.Y) / (1000.0 + (ease * 18000.0)); // constants are just guesses for now, should probably be depending on screen resolution and _maxStretchPercentage?
 
-            if (_stretch_y >= 1.05)
-                _stretch_y = 1.05; // max 5% stretching
+            if (_stretch_y >= _maxStretchPercentage)
+                _stretch_y = _maxStretchPercentage;
 
             e.Handled = true;
             e.ShouldEndScrollGesture = true;
 
             _stretchedBottom = e.Delta.Y >= 0;
             StretchVertically(_stretch_y);
-        }
-
-        public void OnScrollGestureEnded(object? sender, ScrollGestureEndedEventArgs e)
-        {
-            ReleaseStretch();
         }
 
         private void ResetStretch()
@@ -118,31 +117,31 @@ public class StretchEdgeEffect : Decorator
 
             _isReleasing = true;
 
-            TimeSpan duration = TimeSpan.FromMilliseconds(350);
-            Avalonia.Animation.Easings.Easing easing = new Avalonia.Animation.Easings.CubicEaseOut();
+            
 
             var v = (ElementComposition.GetElementVisual(_child) as CompositionContainerVisual)!;
             var releaseAnimationGroup = v.Compositor.CreateAnimationGroup();
             var scalingAnimation = v.Compositor.CreateVector3KeyFrameAnimation();
-            scalingAnimation.Duration = duration;
+            scalingAnimation.Duration = releaseAnimationDuration;
             scalingAnimation.InsertKeyFrame(0f, v.Scale);
-            scalingAnimation.InsertKeyFrame(1f, Vector3.One, easing);
+            scalingAnimation.InsertKeyFrame(1f, Vector3.One, _easing);
             scalingAnimation.Target = nameof(v.Scale);
             releaseAnimationGroup.Add(scalingAnimation);
 
             if (_stretchedBottom)
             {
                 var offsetAnimation = v.Compositor.CreateVector3KeyFrameAnimation();
-                offsetAnimation.Duration = duration;
+                offsetAnimation.Duration = releaseAnimationDuration;
                 offsetAnimation.InsertKeyFrame(0f, v.Offset);
-                offsetAnimation.InsertKeyFrame(1f, Vector3.Zero, easing);
+                offsetAnimation.InsertKeyFrame(1f, Vector3.Zero, _easing);
                 offsetAnimation.Target = nameof(v.Offset);
                 releaseAnimationGroup.Add(offsetAnimation);
             }
+
             v.StartAnimationGroup(releaseAnimationGroup);
 
             // question: another way to know when releaseAnimationGroup is completed?
-            DispatcherTimer.RunOnce(ResetStretch, duration);
+            DispatcherTimer.RunOnce(ResetStretch, releaseAnimationDuration);
         }
     }
 }
